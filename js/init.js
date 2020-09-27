0af3c4f5557e1d57b51742a6a9ebadedf42296ea
Jen Jayme <jeniferjayme@gmail.com>
2020-09-26 23:48:55 -0700

    Added script to get form inputs

--- js/init.js
+++ js/init.js
@@ -1,3 +1,34 @@
+var submitBtn = $('#submitBtn');
+
+function getInputs() {
+  'use strict';
+  var input, firstName, lastName, petName, petType, phone, email, date, time;
+
+    // alert('Submit button clicked');
+    firstName = $('#firstName').val();
+    lastName = $('#lastName').val();
+    phone = $('#phone').val();
+    email = $('#email').val();
+    petName = $('#petName').val();
+    petType = $('#petType').val();
+    date = $('#date').val();
+    time = $('#time').val();
+    input = {
+      'firstName': firstName,
+      'lastName': lastName,
+      'phone': phone,
+      'email': email,
+      'petName': petName,
+      'petType': petType,
+      'date': date,
+      'time': time
+    }
+    console.log("Input:", input)
+    return input;
+}
+
+
+
 (function($){
   $(function(){
 
@@ -11,6 +42,10 @@
     $('.datepicker').datepicker();
     $('.timepicker').timepicker();
 
+    submitBtn.on('click', function (event) {
+      event.preventDefault();
+      var input = getInputs();
+    })    
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
