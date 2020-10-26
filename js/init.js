80ec3344df705c8f5f20c985acbf39e9b4f1056d
Jen Jayme <jeniferjayme@gmail.com>
2020-10-26 14:20:02 -0700

    Fixed sidenav menu functionality

--- js/init.js
+++ js/init.js
@@ -27,17 +27,10 @@ function getInputs() {
     return input;
 }
 
-
-
-(function($){
-  $(function(){
+$(document).ready(function(){
 
     $('.sidenav').sidenav();
     $('.parallax').parallax();
-    $('.carousel').carousel().carousel({
-      shift: -100,
-      dist: -100,
-    });
     $('select').formSelect();
     $('.datepicker').datepicker();
     $('.timepicker').timepicker();
@@ -50,4 +43,3 @@ function getInputs() {
     })    
 
   }); // end of document ready
-})(jQuery); // end of jQuery name space
