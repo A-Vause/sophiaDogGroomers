4c979baff4a128a6f9dcb6b32fe4ad6a26f4785b
Jen Jayme <jeniferjayme@gmail.com>
2020-10-27 11:28:09 -0700

    Deactivated book.html until working properly

---
+++ contact/test.php
@@ -0,0 +1,47 @@
+<?PHP
+/*
+    Contact Form from HTML Form Guide
+    This program is free software published under the
+    terms of the GNU Lesser General Public License.
+    See this page for more info:
+    http://www.html-form-guide.com/contact-form/php-contact-form-tutorial.html
+*/
+require_once("../include/fgcontactform.php");
+
+$formproc = new FGContactForm();
+
+//1. Add your email address here.
+//You can add more than one receipients.
+$formproc->AddRecipient('jeniferjayme@gmail.com'); //<<---Put your email address here
+
+
+//2. For better security. Get a random tring from this link: http://tinyurl.com/randstr
+// and put it here
+$formproc->SetFormRandomKey('bRL5AfylGlOUwix');
+
+
+if(isset($_POST['submitted']))
+{
+   if($formproc->ProcessForm())
+   {
+        $formproc->RedirectToURL("thank-you.php");
+   }
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Test Page</title>
+    <link rel="STYLESHEET" type="text/css" href="./contact.css" />
+    <script type='text/javascript' src='../scripts/gen_validatorv31.js'></script>
+</head>
+<body>
+    
+    <h1>This is a test html page</h1>
+
+</body>
+</html>
